bc76cfe1066378016d09d98ae0c8a61cfe183f1a
Kazibwe Julius Junior <kazibwejuliusjunior@gmail.com>
2019-03-26 05:50:25 +0300

    Blog post with seo friendly url

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
+use App\Post;
 
 class RouteServiceProvider extends ServiceProvider
 {
@@ -26,6 +27,11 @@ class RouteServiceProvider extends ServiceProvider
         //
 
         parent::boot();
+        Route::bind('post', function($slug) {
+            return Post::published()->where('slug', $slug)->first();
+        });
+       
+        
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -18,9 +18,10 @@ Route::get('/', [
     ]);
 
 
-Route::get('blog.show', function () {
-    return view('blog.show');
-});
+    Route::get('/blog.{post}', [
+        'uses' => 'BlogController@show',
+        'as'   => 'blog.show'
+    ]);
 
 Auth::routes();
 
